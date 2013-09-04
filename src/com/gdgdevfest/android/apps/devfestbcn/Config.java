cd98b3648abfe048e80bc3b30b3dfbe9401ee35c
cdsap <inaki.seri@gmail.com>
2013-09-04 14:34:53 +0200

    Package com.gdgdevfest.android.apps.devfestbcn for Config.java

--- src/com/gdgdevfest/android/apps/devfestbcn/Config.java
+++ src/com/gdgdevfest/android/apps/devfestbcn/Config.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.google.android.apps.iosched;
+package com.gdgdevfest.android.apps.devfestbcn;
 
 public class Config {
     // General configuration
